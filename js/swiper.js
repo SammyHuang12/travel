14352076d831712d68176a9424ae22fc3ad4ffa0
SammyHuang12 <jessie_hkimo@yahoo.com.tw>
2026-02-06 00:06:56 +0800

    新增 banner swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,23 @@
 console.log("swiper js");
+
+const banner_swiper = new Swiper(".banner-swiper", {
+    slidesPerView: 'auto',
+    speed: 1200,
+    autoplay: {
+        delay: 4000, // 延遲 5 秒
+        disableOnInteraction: false, // 用戶操作後是否停止 (false 為不停止)
+    },
+    loop: true, // 循環播放，確保無限輪播
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true,
+    },
+});
+
 const travel_swiper = new Swiper(".travel-swiper", {
     slidesPerView: 'auto',
     spaceBetween: 24,
